d47c6293636e59931d41123521f79acca50497b9
St-Nischal <nishalpoudel50@gmail.com>
2025-04-13 20:45:07 +0100

    Created a more comprlehensive 3d render

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-/***
- * In this class I am going to be building a 3d engine.
- * This main file is going to be where everything is going to be run
- * In this file, I am going to be creating
- * ***/
+/*
+  In this class I am going to be building a 3d engine.
+  This main file is going to be where everything is going to be run
+  In this file, I am going to be creating
+ */
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
 
 // Class used to create a Vertex
-//Vertex is simply a structure to store our three coordinates (X, Y and Z)
+// is simply a structure to store our three coordinates (X, Y and Z)
 class Vertex {
     double x;
     double y;
@@ -42,6 +43,33 @@ class Triangle{
         this.v3 = v3;
         this.color = color;
     }
+}
+
+class   Matrix3{
+    double[] values;
+    Matrix3(double[] values){
+        this.values =values;
+    }
+    Matrix3 multiply(Matrix3 other) {
+        double[] results = new double[9];
+        for (int row = 0; row<3; row++){
+            for(int col =0; col<3; col++){
+                for( int i = 0; i <3; i++){
+                    results[row * 3 + col] += this.values[row*3 +i] * other.values[i*3+col];
+                }
+            }
+        }
+        return new Matrix3(results);
+    }
+
+    Vertex transform(Vertex in){
+        return new Vertex(
+                in.x * values[0] + in.y * values[3] + in.z * values[6],
+                in.x * values[1] + in.y * values[4] + in.z * values[7],
+                in.x * values[2] + in.y * values[5] + in.z * values[8]
+        );
+    }
+
 }
 
 public class Main {
@@ -80,30 +108,102 @@ public class Main {
         JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL,-90,90,0);
         pane.add(pitchSlider, BorderLayout.EAST);
 
+
         //panel to display render results
-        JPanel renderPancel = new JPanel(){
+        JPanel renderPanel = new JPanel(){
             public void paintComponent(Graphics g){
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0,0, getWidth(),getHeight());
 
+                double heading = Math.toRadians(headingSlider.getValue());
+                Matrix3 headingTransform = new Matrix3(new double[] {
+                        Math.cos(heading), 0, Math.sin(heading),
+                        0, 1, 0,
+                        -Math.sin(heading), 0, Math.cos(heading)
+                });
+                double pitch = Math.toRadians(pitchSlider.getValue());
+                Matrix3 pitchTransform = new Matrix3(new double[] {
+                        1, 0, 0,
+                        0, Math.cos(pitch), Math.sin(pitch),
+                        0, -Math.sin(pitch), Math.cos(pitch)
+                });
+                Matrix3 transform = headingTransform.multiply(pitchTransform);
+
+                BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+                for( int q = 0; q < zBuffer.length; q++){
+                    zBuffer[q] = Double.NEGATIVE_INFINITY;
+                }
+
+
                 //All the render will happen here
 
                 //Displaying the triangle above as wireframe
-                g2.translate(getWidth()/2, getHeight()/2);
-                g2.setColor(Color.WHITE);
-                for (Triangle t : tris){
-                    Path2D path = new Path2D.Double();
-                    path.moveTo(t.v1.x, t.v1.y);
-                    path.lineTo(t.v2.x, t.v2.y);
-                    path.lineTo(t.v3.x, t.v3.y);
-                    path.closePath();
-                    g2.draw(path);
+                for(Triangle t: tris){
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
+
+                    // Since we are not using Graphics2D anymore
+                    // we have to do translate manually
+
+                    v1.x += (double) getWidth() / 2;
+                    v1.y += (double) getHeight() / 2;
+                    v2.x += (double) getWidth() / 2;
+                    v2.y += (double) getHeight() / 2;
+                    v3.x += (double) getWidth() / 2;
+                    v3.y += (double) getHeight() / 2;
+
+                    //compute rectangular bounds for triangle
+                    int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+                    int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+
+                    int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+                    int maxY = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+
+                    double trianglArea = (v1.y -v3.y) * (v2.x -v3.x) + (v2.y -v3.y) * (v3.x -v1.x);
+
+                    for( int y = minY; y <= maxY; y++){
+                        for(int x = minX; x <= maxX; x++){
+
+                            double b1 = ((y -v3.y) * (v2.x -v3.x) + (v2.y -v3.y) * (v3.x -x)) / trianglArea;
+
+                            double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / trianglArea;
+
+                            double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / trianglArea;
+
+                            if( b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1){
+                                img.setRGB(x, y, t.color.getRGB());
+                            }
+
+
+                            double depth = b1* v1.z + b2 * v2.z + b3 * v3.z;
+                            int zIndex = y *img.getWidth() + x;
+                            if (zBuffer[zIndex] < depth) {
+                                img.setRGB(x, y, t.color.getRGB());
+                                zBuffer[zIndex] = depth;
+                            }
+
+                        }
+
+
+
+
+                    }
+
                 }
+
+                g2.drawImage(img, 0, 0, null);
+
+
 
             }
         };
-        pane.add(renderPancel, BorderLayout.CENTER);
+        pane.add(renderPanel, BorderLayout.CENTER);
+        headingSlider.addChangeListener(e -> renderPanel.repaint());
+        pitchSlider.addChangeListener(e -> renderPanel.repaint());
 
         frame.setSize(400, 400);
         frame.setVisible(true);
